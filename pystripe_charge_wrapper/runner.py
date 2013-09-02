a81daeef1d6936cb57dce2a74667fcc26d4c5f14
Mike Lopez <dev@scidentify.info>
2013-09-02 16:17:45 -0400

    base test runner

---
+++ pystripe_charge_wrapper/runner.py
@@ -0,0 +1,6 @@
+from test_charges import TestStripeCharges
+
+if __name__ == '__main__':
+    suite = TestSuite()
+    suite.addTest(TestStripeCharges('test_charge'))
+    TextTestRuner(verbosity=2).run(suite)
