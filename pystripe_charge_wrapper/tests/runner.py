04239046a764f1bebe324f3b4111714f03b02882
Mike Lopez <dev@scidentify.info>
2013-09-02 16:20:08 -0400

    just incase...

--- pystripe_charge_wrapper/tests/runner.py
+++ pystripe_charge_wrapper/tests/runner.py
@@ -1,6 +1,14 @@
+import os
+import sys
+
+PATH_APPEND = os.path.realpath(os.path.dirname(__file__))
+#sys.path.append('../')
+
 from test_charges import TestStripeCharges
 
 if __name__ == '__main__':
     suite = TestSuite()
     suite.addTest(TestStripeCharges('test_charge'))
     TextTestRuner(verbosity=2).run(suite)
+
+
